75bc42ec722b6666628cfa33b59aee1868931d45
EOussama <oussama_gtacj@live.fr>
2023-11-18 23:54:30 +0100

    ACP

--- src/main.R
+++ src/main.R
@@ -1,2 +1,23 @@
-data <- read.csv("src/eaux.csv")
-View(data)
+data <- read.csv("src/eaux.csv", header = TRUE)
+
+norm <- function (data) {
+  return((length(data) - 1) / length(data))
+}
+
+pvar <- function (data) {
+  return(var(data) * norm(data))
+}
+
+psd <- function (data) {
+  return(sd(data) * sqrt(norm(data)))
+}
+
+pcov <- function (x, y) {
+  return(cov(x, y) * norm(x))
+}
+
+dt <- data[, 11:14]
+dt <- sapply(dt, function(x) as.numeric(gsub(",", ".", x)))
+
+pca_result <- prcomp(dt)
+print(summary(pca_result))
