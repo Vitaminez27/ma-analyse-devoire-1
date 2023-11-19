85073019110e5df60b00d69ed7852c779ae159dd
EOussama <oussama_gtacj@live.fr>
2023-11-19 16:27:54 +0100

    Question 2

--- src/main.R
+++ src/main.R
@@ -1,25 +1,15 @@
 data <- read.csv("src/eaux.csv", header = TRUE)
+View(data)
 
-norm <- function (data) {
-  return((length(data) - 1) / length(data))
-}
+# 1 - Est-il pertinent, dans le cas présent, de recourir à une Analyse en Composantes Principales (ACP) pour réduire le nombre de variables ? Justifier.
+print("1 - Oui, pour réduire la dimensionnalité. Cela peut faciliter l'analyse en éliminant le bruit et en mettant en évidence les structures sous-jacentes.")
 
-pvar <- function (data) {
-  return(var(data) * norm(data))
-}
+# 2 - Est-il nécessaire de centrer-réduire les données avant de procéder à une ACP ? Justifier.
+print("2 - Oui, il est généralement recommandé de centrer et de réduire les données avant de procéder à une Analyse en Composantes Principales (ACP). Cette étape, souvent appelée standardisation, est importante pour plusieurs raisons, et voici quelques ")
 
-psd <- function (data) {
-  return(sd(data) * sqrt(norm(data)))
-}
+# dt <- data[, 6:14]
+# dt <- sapply(dt, function(x) as.numeric(gsub(",", ".", x)))
 
-pcov <- function (x, y) {
-  return(cov(x, y) * norm(x))
-}
+# pca_result <- prcomp(dt, scale. = TRUE)
+# print(summary(pca_result))
 
-dt <- data[, 6:14]
-dt <- sapply(dt, function(x) as.numeric(gsub(",", ".", x)))
-
-pca_result <- prcomp(dt, scale. = TRUE)
-print(summary(pca_result))
-
-# 1 - Oui, pour réduire la dimensionnalité. Cela peut faciliter l'analyse en éliminant le bruit et en mettant en évidence les structures sous-jacentes.
