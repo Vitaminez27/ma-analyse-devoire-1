bf4f6caea0765908c7b92238db90ac6d8c3932f4
EOussama <oussama_gtacj@live.fr>
2023-11-20 13:40:29 +0100

    Question 5

--- src/main.R
+++ src/main.R
@@ -13,9 +13,15 @@ print("3 - Nous devrions exclure NOM, ACRO, PAYS, TYPE, et PG parce qu'ils ne so
 # 4 - Pour la suite du problème, la dernière variable (la masse moyenne des ions) sera considérée dans l’ACP comme “ variable supplémentaire”. Pouvez-vous argumenter ce choix ?
 print("4 - La masse ionique moyenne fournit une mesure agrégée de la composition ionique de l'eau. En incluant cette variable, nous pouvons synthétiser l'information contenue dans les différentes concentrations d'ions (CA, MG, NA, K, SUL, NO3, HCO3, CL) en une seule variable, ce qui peut simplifier l'interprétation des résultats de l'ACP.")
 
-# dt <- data[, 6:14]
-# dt <- sapply(dt, function(x) as.numeric(gsub(",", ".", x)))
+# 5 - Faire une ACP. Quel pourcentage de variabilité est expliqué par les deux premières composantes ?
+data_numerique <- sapply(dt, function(x) as.numeric(gsub(",", ".", x)))
+data_cr <- scale(dt, center=TRUE, scale=TRUE)
 
-# pca_result <- prcomp(dt, scale. = TRUE)
-# print(summary(pca_result))
+data_acp <- prcomp(data_cr, scale.=TRUE)
 
+variance_expliquee <- data_acp$sdev^2
+pourcentage_var_expliquee <- variance_expliquee / sum(variance_expliquee) * 100
+
+cat("5 - Pourcentage de variabilité expliquée par la 1ère composante :", round(pourcentage_var_expliquee[1], 2), "%\n")
+cat("  - Pourcentage de variabilité expliquée par la 2ème composante :", round(pourcentage_var_expliquee[2], 2), "%\n")
+cat("  - Pourcentage totale des deux première composantes est ", round(pourcentage_var_expliquee[1] + pourcentage_var_expliquee[2], 2), "%\n")
